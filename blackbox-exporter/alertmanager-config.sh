67d9fbfb5e286f3875e71c07eee29268e27e5440
Alexander Giesa <alexander@kubernauts.de>
2020-05-29 11:49:25 +0200

    init commit

---
+++ blackbox-exporter/alertmanager-config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+kubectl delete secret alertmanager-promo-prometheus-operator-alertmanager --namespace=default
+
+sleep 5
+
+kubectl create secret generic alertmanager-promo-prometheus-operator-alertmanager --from-file=alertmanager.yaml --namespace=default 
+kubectl label secret alertmanager-promo-prometheus-operator-alertmanager app=prometheus-operator-alertmanager --namespace=default
+kubectl label secret alertmanager-promo-prometheus-operator-alertmanager release=promo --namespace=default
+
+# for debuging set the loglevel of the alertmanager to debug and take a look at the logs
