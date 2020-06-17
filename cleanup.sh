f6ce5d37fb25091fac264e22175a6049b52db606
Alexander Giesa <alexander@kubernauts.de>
2020-06-17 17:58:38 +0200

    cleanup enhancement

--- cleanup.sh
+++ cleanup.sh
@@ -9,6 +9,35 @@ NODES+=$MASTER
 NODES+=" "
 NODES+=$WORKERS
 
+cleanupAnsw="y"
+read -p  "Do you want to clean your /etc/hosts from multipass entries (y/n)?(default:y) promt with [ENTER]:" input
+cleanupAnsw="${input:-$cleanupAnsw}"
+
+if [ $cleanupAnsw == 'y' ];
+then
+  echo "We need to write the host entries on your local machine to /etc/hosts"
+  echo "Please provide your sudo password:"
+
+  # backup before cleanup
+  cp /etc/hosts hosts.cleanup.backup
+
+  # seach for existing multipass config
+  exists=$(grep -n "####### multipass hosts start ##########" hosts | awk -F: '{print $1}' | head -1)
+  # check if var is empty
+  if test -z "$exists" 
+  then
+    exists=0
+  fi
+
+  # cut existing config
+  if (("$exists" > "0")) ; then
+    start=$(grep -n "####### multipass hosts start ##########" hosts | awk -F: '{print $1}' | head -1)
+    ((start=start-1))
+    end=$(grep -n "####### multipass hosts end   ##########" hosts | awk -F: '{print $1}' | head -1)
+    sed -i '' ${start},${end}d /etc/hosts
+  fi
+fi
+
 # Stop then delete nodes
 for NODE in ${NODES}; do multipass stop ${NODE} && multipass delete ${NODE}; done
 # Free discspace
