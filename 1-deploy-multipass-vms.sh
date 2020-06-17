0d1de335af4f39e75f074244c244cd2c39a2f275
Alexander Giesa <alexander@kubernauts.de>
2020-06-17 17:51:15 +0200

    make node parameter configurable

--- 1-deploy-multipass-vms.sh
+++ 1-deploy-multipass-vms.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 nodeCount=2
-read -p  "How many worker nodes do you want?(default:2) promt with [ENTER]:" input
-nodeCount="${input:-$nodeCount}"
+read -p  "How many worker nodes do you want?(default:2) promt with [ENTER]:" inputNode
+nodeCount="${inputNode:-$nodeCount}"
+cpuCount=2
+read -p  "How many cpus do you want per node?(default:2) promt with [ENTER]:" inputCpu
+cpuCount="${inputCpu:-$cpuCount}"
+memCount=4
+read -p  "How many gigabyte memory do you want per node?(default:4) promt with [ENTER]:" inputMem
+memCount="${inputMem:-$memCount}"
+diskCount=10
+read -p  "How many gigabyte diskspace do you want per node?(default:10) promt with [ENTER]:" inputDisk
+diskCount="${inputDisk:-$diskCount}"
 
 MASTER=$(echo "k3s-master ") && WORKER=$(eval 'echo k3s-worker{1..'"$nodeCount"'}')
 NODES+=$MASTER
 NODES+=$WORKER
 
 # Create containers
-for NODE in ${NODES}; do multipass launch --name ${NODE} --cpus 2 --mem 4G --disk 10G; done
+for NODE in ${NODES}; do multipass launch --name ${NODE} --cpus ${cpuCount} --mem ${memCount}G --disk ${diskCount}G; done
 
 # Wait a few seconds for nodes to be up
 sleep 5
